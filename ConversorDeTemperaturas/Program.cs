4402ebec4f89ee9162b2c498e8d6b02efbea6d4d
Lucas <lucasgunner3@gmail.com>
2021-04-17 21:35:35 -0300

    Algoritmo pronto

--- ConversorDeTemperaturas/Program.cs
+++ ConversorDeTemperaturas/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace ConversorDeTemperaturas
 {
@@ -6,7 +7,16 @@ namespace ConversorDeTemperaturas
     {
         static void Main(string[] args)
         {
-            
+            double c, f, k;
+
+            Console.WriteLine("################################ Bem-vindo(a) ao conversor de temperatura ################################");
+            Console.Write("Digite a temperatura para converter: ");
+            c = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            f = c * 9/5 + 32;
+            k = c + 273.15;
+            Console.WriteLine("Conversão em fahrenheit\t|\tConversão em Kelvin");
+            Console.WriteLine($"{f.ToString("F2",CultureInfo.InvariantCulture)}\t\t\t|\t\t\t{k.ToString("F2",CultureInfo.InvariantCulture)}");
+            Console.ReadKey();
         }
     }
 }
